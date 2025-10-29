1a2c675f41ad961cf42d3d98823b07d0984191ae
Jhonnatan VT Wolf <133080033+FranciscoJVT0104@users.noreply.github.com>
2025-10-29 08:46:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ '¡¡FELIZ', 'CUMPLEAÑOS!!' , 'VANESSA ZURITA'],
+			strings: [ '¡¡FELIZ', 'CUMPLEAÑOS!!' , 'JEFA JESSICA EGOAVIL'],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
